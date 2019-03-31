20c36e6675f2354075b49654d45e8a2d1dede8e5
syu <1838653805@qq.com>
2019-03-31 20:30:46 +0800

    merge

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -11,6 +11,7 @@ import { SafeAreaView } from 'react-navigation';
 import { Divider } from 'react-native-elements'
 import {inject,observer} from 'mobx-react'
 import {sty} from '../../config/styles'
+import LinearGradient from 'react-native-linear-gradient';
 
 @inject(["homeStore"])
 @observer // 监听当前组件
@@ -32,10 +33,32 @@ class Home extends  Component{
           }}>
         <Text>gogogo</Text>
         </TouchableOpacity>
+        <LinearGradient colors={['#4c669f', 'red', '#192f6a']} style={styles.linearGradient}>
+  <Text style={styles.buttonText}>
+    Sign in with Facebook
+  </Text>
+</LinearGradient>
 
           </SafeAreaView>
       )
   }
 
 }
+const styles=StyleSheet.create({
+    linearGradient: {
+        // flex: 1,
+        height:200,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 5
+      },
+      buttonText: {
+        fontSize: 18,
+        fontFamily: 'Gill Sans',
+        textAlign: 'center',
+        margin: 10,
+        color: '#ffffff',
+        backgroundColor: 'transparent',
+      },
+})
 export default Home
